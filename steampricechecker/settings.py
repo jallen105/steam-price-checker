7db91a25b806a08a9fbeccd46361d6513f09a85a
Jonathan <jonathanallen105@gmail.com>
2025-10-20 12:56:50 -0400

    setup a home/login and sign up page. login and logout functionality is working.

--- steampricechecker/settings.py
+++ steampricechecker/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'steampricechecker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'steamwatchlist',
     }
 }
 
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
